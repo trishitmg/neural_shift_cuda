9613834af46c1b5ae1e93ffe98d6a35028ab3b65
007Trishit <mukherjeetrishit@gmail.com>
2026-07-04 15:29:46 +0000

    init updated

--- neural_shift_cuda/__init__.py
+++ neural_shift_cuda/__init__.py
@@ -3,18 +3,22 @@ from .ops import (
     shift_gather,
     pair_gather,
     accumulate_uz,
+    accumulate_uz_scalar,
     shift_gather_reference,
     pair_gather_reference,
     accumulate_uz_reference,
+    accumulate_uz_scalar_reference,
 )
 from .metropolis import metropolis_aggregate
-__version__ = "0.1.0"
+__version__ = "0.4.0"
 __all__ = [
     "shift_gather",
     "pair_gather",
     "accumulate_uz",
+    "accumulate_uz_scalar",
     "shift_gather_reference",
     "pair_gather_reference",
     "accumulate_uz_reference",
+    "accumulate_uz_scalar_reference",
     "metropolis_aggregate",
 ]
