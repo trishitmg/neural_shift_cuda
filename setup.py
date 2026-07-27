d750c98ab2fb84ba67f80df9cd72125375309096
007Trishit <mukherjeetrishit@gmail.com>
2026-07-27 22:12:56 +0000

    updated setup for newer cards

--- setup.py
+++ setup.py
@@ -37,13 +37,12 @@ setup(
                     "--use_fast_math",
                     # We use atomicAdd on float and double; double atomicAdd
                     # requires SM 6.0 or newer (Pascal+). Most modern GPUs.
-                    "-gencode=arch=compute_60,code=sm_60",
-                    "-gencode=arch=compute_70,code=sm_70",
-                    "-gencode=arch=compute_75,code=sm_75",
                     "-gencode=arch=compute_80,code=sm_80",
                     "-gencode=arch=compute_86,code=sm_86",
                     "-gencode=arch=compute_89,code=sm_89",
                     "-gencode=arch=compute_90,code=sm_90",
+                    "-gencode=arch=compute_120,code=sm_120",
+                    "-gencode=arch=compute_120,code=compute_120",
                 ],
             },
         )
